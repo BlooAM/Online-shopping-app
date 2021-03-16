a3e0744a6e808cd68b6f6c220da96163ea7554f1
Adam Marek <adam3200@vp.pl>
2021-03-16 08:49:45 +0100

    tests for service layer

---
+++ app/test_services.py
@@ -0,0 +1,38 @@
+from app import model
+from app.repository import FakeRepository
+
+
+class FakeSession:
+    def __init__(self):
+        self.committed = False
+
+    def commit(self):
+        self.commited = True
+
+
+def test_commits():
+    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
+    batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
+    repo = FakeRepository([batch])
+    session = FakeSession()
+
+    services.allocate(line, repo, session)
+    assert session.committed is True
+
+
+def test_returns_allocation():
+    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
+    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    result = services.allocate(line, repo, FakeSession())
+    assert result == "b1"
+
+
+def test_error_for_invalid_sku():
+    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
+    batch = model.Batch("b1", "AREALSKU", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    with pytest.raises(services.InvalidSku, match="Invalid name of SKU: NONEXISTENTSKU"):
+        services.allocate(line, repo, FakeSession())
