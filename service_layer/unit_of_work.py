220f2ff1605c09df9c4ea72310851b54c0660111
Adam Marek <adam3200@vp.pl>
2021-04-08 09:35:52 +0200

    SqlAlchemy unit of work

--- service_layer/unit_of_work.py
+++ service_layer/unit_of_work.py
@@ -1,6 +1,14 @@
 from collections import abc
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from domain import repository
+import config
+from adapters import repository
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(bind=create_engine(
+    config.get_postgres_uri(),
+))
 
 
 class AbstractUnitOfWork(abc.ABC):
@@ -16,3 +24,23 @@ class AbstractUnitOfWork(abc.ABC):
     @abc.abstractmethod
     def rollback(self):
         raise NotImplementedError
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.batches = repository.SqlAlchemyRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.clos()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
