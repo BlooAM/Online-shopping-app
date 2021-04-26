ff140c7a3be7b9aab58645cd70dfa599879992d5
Adam Marek <adam3200@vp.pl>
2021-04-26 08:34:51 +0200

    split handlers between event and command handlers

--- service_layer/messagebus.py
+++ service_layer/messagebus.py
@@ -1,22 +1,62 @@
 import email
+from typing import List
 
-from domain import events
+from domain import events, commands
 from service_layer import unit_of_work, handlers
 
 
-def handle(event: events.Event, uow: unit_of_work.AbstractUnitOfWork):
-    queue = [event]
+def handle(message: Message, uow: unit_of_work.AbstractUnitOfWork):
+    results = []
+    queue = [message]
     while queue:
         event = queue.pop(0)
-        for handler in HANDLERS[type(event)]:
-            handler(event, uow=uow)
-            queue.extend(uow.collent_new_events())
+        if isinstance(message, events.Event):
+            handle_event(message, queue, uow)
+        elif isinstance(message, commands.Command):
+            cmd_result = handle_command(message, queue, uow)
+            results.append(cmd_result)
+        else:
+            raise Exception(f'{message} was not an Event or Command')
     return results
 
 
-HANDLERS = {
-    events.BatchCreated: [handlers.add_batch],
-    events.BatchQuantityChanged: [handlers.change_batch_quantity],
-    events.AllocationRequired: [handlers.allocate],
-    events.OutOfStock: [handlers.send_out_of_stock_notification],
+def handle_event(
+        event: events.Event,
+        queue: List[Message],
+        uow: unit_of_work.AbstractUnitOfWork
+):
+    for handler in EVENT_HANDLERS[type(event)]:
+        try:
+            logger.debug('handling event %s with handler %s', event, handler)
+            handler(event, uow=uow)
+            queue.extend(uow.collect_new_events())
+        except Exception:
+            logger.exception('Exception handling event %s', event)
+            continue
+
+
+def handle_command(
+        command: commands.Command,
+        queue: List[Message],
+        uow: unit_of_work.AbstractUnitOfWork
+):
+    logger.debug('Handling command %s', command)
+    try:
+        handler = COMMAND_HANDLERS[type(command)]
+        result = handler(command, uow=uow)
+        queue.extend(uow.collect_new_events())
+        return result
+    except Exception:
+        logger.Exception(f'Exception handling command %s', command)
+        raise Exception
+
+
+COMMAND_HANDLERS = {
+    events.OUT: [handlers.send_out_of_stock_notification],
+}
+
+COMMAND_HANDLERS = {
+    commands.Allocate: handlers.allocate,
+    commands.CreateBatch: handlers.add_batch,
+    commands.ChangeBatchQuantity: handlers.change_batch_quantity,
 }
